9416267f9fb7a37c98627042a8afdba78bddbb30
AntonioBarandao <antoniobarandao232@gmail.com>
2024-08-05 15:00:20 -0400

    added controllers and updated models

--- index.js
+++ index.js
@@ -1,11 +1,72 @@
 const express = require("express");
 const app = express();
+const sequelize = require("./setup.js");
+
+const eventRoutes = require("./routes/eventRoutes.js");
+const userRoutes = require("./routes/userRoutes.js")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use("/events", eventRoutes);
+app.use("/login",userRoutes);
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
+(async () => {
+  try {
+    await sequelize.sync();
+  } catch (error) {
+    console.error("Failed to synchronize models:", error);
+  }
+})();
+
+app.get('*', (_, res) => {
+  res.status(404).send("Error 404; page not found")
+}); // Page not found error redirect
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const express = require("express");
+const app = express();
 const port = 3000;
 const path = require ('path');
 const { fileURLToPath }  = require ('url');
 const { dirname } = require ('path');
 const sequelize = require("/home/strelizia/Library_Helper-Events/setup.js");
 const sqlite = require ('sqlite')
+const sqlite3 = require ('sqlite3')
 const bodyParser = require("body-parser")
 multer = require("multer")
 
@@ -21,8 +82,8 @@ app.get('/eventform',(req, res) => {
 })
 
 app.post('/eventform', (req, res) => {
-  let db = new sqlite.Database("/home/strelizia/Library_Helper-Events/test.db");
-  db.run(`INSERT INTO dummy (email, txt) VALUES (?,?)`, [
+  let db = new sqlite3.Database("/home/strelizia/Library_Helper-Events/test.db");
+  db.run("INSERT INTO dummy (NULL,'firstName','lastName','phoneNumber','email','descrip') VALUES('antonio','barandao','','','','')", [
     req.body.email, req.body.txt
     ], function (err) {
       if (err) { console.log(err); }
@@ -59,3 +120,4 @@ app.listen(3141, () => {
     console.error("Failed to synchronize models:", error);
   }
 })();
+*/
